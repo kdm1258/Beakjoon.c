2348a139402403f73d173dfe22173eea95248b1c
kdm1258 <kdm1258@naver.com>
2025-02-03 02:59:30 +0900

    1806 sol

---
+++ 1806.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;   
+using ll = long long;
+int n;
+int s;
+ll arr[101010];
+int sol(){
+    int res=INT_MAX;
+    int i = 1, j = 0;
+    while(i <= n){
+        if(arr[i] - arr[j] >= s){
+            if(res > i-j){
+                res = i-j;
+            } 
+            j++;
+        }
+        else{
+            i++;
+        }
+    }
+    if(i==n+1 && j==0) return 0;
+    return res;
+}
+int main(void){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n>>s;
+    for(int i = 1 ; i <= n ; i++){
+        cin>>arr[i];
+        arr[i] += arr[i-1];
+    }
+    cout<<sol();
+    
+}
--- comstruct/ex.s
+++
@@ -1,2 +0,0 @@
-addi x1, x2, 13
-addi x3, x4, -2
