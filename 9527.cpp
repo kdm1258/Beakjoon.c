8ec7ed467b8e5c3861f5b438127c03ed6d4e811b
kdm1258 <kdm1258@naver.com>
2025-02-03 16:24:03 +0900

    fix MemExceed & TimeExceed

--- 9527.cpp
+++ 9527.cpp
@@ -1,35 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-ll S[60];
-ll a, b;
-/*string toBi(ll n) {
-    string binary = "";
-    while (n > 0) {
-        binary = (n % 2 == 0 ? "0" : "1") + binary;
-        n /= 2;
-    }
-    return binary.empty() ? "0" : binary;
-}*/
 
-ll sol(ll n){
+ll S[55];
+ll a, b;
+
+ll sol(ll n) {
     // 1~n까지 1의 개수 return
-    //cout<<toBi(n)<<"\n";
-    int l = log2(n);
-    if(n==0)return 0;
-    return S[l] + n - pow(2,l) + 1 + sol(n & ~(1<<l)); //bitmasking
+    ll res = 0;
+    while (n > 0) {
+        int l = log2(n);
+        res += S[l] + n - (1LL << l) + 1LL;
+        n = n & ~(1LL << l);
+    }
+    return res;
 }
-void setS(){
+
+void setS() {
     // S[i]일때 2^i까지의 1의 개수
     S[1] = 1;
-    for(int i = 2 ; i < 60 ; i++){
-        S[i] = S[i-1]*2 + pow(2, i-1);
+    for (int i = 2; i < 55; i++) {
+        S[i] = S[i-1] * 2 + (1LL << (i-1));
     }
 }
-int main(void){
+
+int main(void) {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     setS();
-    cin>>b>>a;
-    cout<<sol(b)-sol(a-1);
+    cin >> b >> a;
+    cout << sol(b) - sol(a - 1);
+    return 0;
 }
