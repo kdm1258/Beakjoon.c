a4c475e46e00efcd84b7b83059e8a87786a1e19a
kdm1258 <kdm1258@naver.com>
2025-02-18 00:15:16 +0900

    7453.cpp 수정

--- 7453.cpp
+++ 7453.cpp
@@ -1,29 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+
 int n;
-int result;
+ll result;
 int A[4000], B[4000], C[4000], D[4000];
-map<int, int> AB;
-map<int, int> CD;
-int main(void){
+vector<ll> AB;
+
+int main(void) {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    cin>>n;
-    for(int i = 0 ; i <n ; i++){
-        cin>>A[i]>>B[i]>>C[i]>>D[i];
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i] >> B[i] >> C[i] >> D[i];
     }
-    for(int i = 0 ; i < n ; i ++){
-        for (int j = 0 ; j <n ; j++){
-            AB[A[i]+B[j]]++;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            AB.push_back(A[i] + B[j]);
         }
     }
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < n ; j++){
-            if(AB.find(-(C[i]+D[j])) != AB.end()){
-                result += AB[-(C[i]+D[j])];
-            }
+
+    sort(AB.begin(), AB.end());
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            ll S = -(C[i] + D[j]);
+            auto lower = lower_bound(AB.begin(), AB.end(), S);
+            auto upper = upper_bound(AB.begin(), AB.end(), S);
+            result += upper - lower;
         }
     }
+
     cout << result << '\n';
+    return 0;
 }
